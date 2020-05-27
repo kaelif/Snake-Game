56cedf7635a951836d61a670a7e59f6e2bfd07a8
kaelif <flanagan.kaeli@gmail.com>
2020-05-27 13:59:42 -0700

    added head

--- main.py
+++ main.py
@@ -9,3 +9,18 @@ wn.title("Snake Game")
 wn.bgcolor("blue")
 wn.setup(width=600, height=600)
 wn.tracer(0)
+
+# Snake head
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("black")
+head.penup()
+head.goto(0, 0)
+head.direction = "stop"
+
+# Main game loop
+while True:
+    wn.update()
+
+wn.mainloop()
