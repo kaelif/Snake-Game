e19fc892620a530463cf89b80c099727a54503f5
kaelif <flanagan.kaeli@gmail.com>
2020-05-27 15:50:17 -0700

    added tail

--- main.py
+++ main.py
@@ -29,6 +29,8 @@ food.color("green")
 food.penup()
 food.goto(0, 100)
 
+segments = []
+
 # Functions
 def go_up():
     head.direction = "up"
@@ -73,11 +75,32 @@ wn.onkeypress(go_left, "a")
 while True:
     wn.update()
 
+    # Check for a collision with the food
     if head.distance(food) < 20:
         # Move food to random place on screen
         x = random.randint(-290, 290)
         y = random.randint(-290, 290)
         food.goto(x, y)
+
+        # Add a segment
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color("grey")
+        new_segment.penup()
+        segments.append(new_segment)
+
+    # Move the end segments first in reverse order
+    for index in range(len(segments) -1, 0,  -1):
+        x = segments[index - 1].xcor()
+        y = segments[index - 1].ycor()
+        segments[index].goto(x, y)
+
+    # Move segments 0 to where the head is
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x, y)
 
     move()
 
