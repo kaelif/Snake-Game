04748afb669a08bf916c85d36b03a40ddf9ae40e
kaelif <flanagan.kaeli@gmail.com>
2020-05-27 13:52:13 -0700

    added background

---
+++ main.py
@@ -0,0 +1,11 @@
+
+import turtle
+# import time
+# import random
+
+# Set up the screen
+wn = turtle.Screen()
+wn.title("Snake Game")
+wn.bgcolor("blue")
+wn.setup(width=600, height=600)
+wn.tracer(0)
